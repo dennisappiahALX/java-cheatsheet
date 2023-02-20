0650620f729456bcf2a026de433d00e1b7313a48
dennisappiahALX <appiahdennis1998@gmail.com>
2023-02-20 15:32:11 +0000

    a Java program to display prime numbers from 1 to n

--- src/DecisionMakingChallenge.java
+++ src/DecisionMakingChallenge.java
@@ -7,6 +7,8 @@ public class DecisionMakingChallenge {
             product();
             compare();
             stringInteger();
+            isPrime();
+            primeNumbers();
     }
 
     public static  void factorial (){
@@ -77,5 +79,62 @@ public class DecisionMakingChallenge {
         int j = 100;
         String l = String.valueOf(j);
         System.out.println(l);
+    }
+
+    public static void isPrime(){
+    /* Prime number is a positive number greater than 1 that has only 2 factors: 1 and itself */
+        /* Write a Java program to checks prime numbers from 1 to n (first n prime numbers)
+        (entered by user)*/
+    //Algorithm:
+    /*
+    - loop through the n items, check if i is a factor n (n is divisible by i)
+     if you find any value which is a factor of itself,
+    increase the count by 1
+    */
+        //user input of number
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your number");
+        int number = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 1; i <= number; i++){
+
+            if (number % i == 0 ){
+                count++;
+            }
+            if (count == 2){
+                System.out.println("prime");
+            }
+            else {
+                System.out.println("not prime");
+            }
+        }
+    }
+
+    public static void primeNumbers (){
+         /* Write a Java program to display prime numbers from 1 to n (first n prime numbers)
+        (entered by user)*/
+
+        //user input of number
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your value number");
+        int number = scanner.nextInt();
+
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+
+            //j= 2, 3, 5, 7, 11
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
